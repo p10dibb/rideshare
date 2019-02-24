9d4eb9657e182c76424dac96316b2065f4a7742d
Diwash Biswa <diwashbiswa5@gmail.com>
2019-02-23 22:13:07 -0800

    Google Map API initialized -- not done with it tho

--- RideShare/app/src/main/java/com/example/rideshare/ProfilePageActivity.java
+++ RideShare/app/src/main/java/com/example/rideshare/ProfilePageActivity.java
@@ -1,14 +1,50 @@
 package com.example.rideshare;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.view.View;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.MenuItem;
+import android.view.Menu;
+import android.widget.Button;
 
 public class ProfilePageActivity extends AppCompatActivity {
+    private Button createRideButton, findRideButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_page);
+
+        createRideButton = (Button) findViewById(R.id.createRideButton);
+        findRideButton = (Button) findViewById(R.id.findRideButton);
+
+        createRideButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                createRideButtonClicked();
+            }
+        });
+
+        findRideButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findRideButtonClicked();
+            }
+        });
+    }
+
+    //segues to Create Ride Page
+    public void createRideButtonClicked()
+    {
+        Intent segueToCreateAccount = new Intent(this, FindRideActivity.class);
+        startActivity(segueToCreateAccount);
+    }
+
+    //segues to Find Ride page
+    public void findRideButtonClicked()
+    {
+        Intent segueToSignInPage = new Intent(this, FindRideActivity.class);
+        startActivity(segueToSignInPage);
     }
 }
